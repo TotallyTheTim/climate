ee8c0f4d5c4e099310590edc3a348270c4c6b14e
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2019-02-12 12:44:24 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+setTimeout(function () {
+    document.getElementById('loading').style.opacity = '0';
+},3000);
+
+window.scrollTo(0, 0);
 
 let bob = document.getElementById('bob');
 let bob_I = 0;
@@ -8,12 +13,26 @@ let jumpies = {
     7 : [2150, 2910]
 };
 
+let boxes = {
+    box1 : [1085,203],
+    box2 : [5291,203]
+};
+
+
+let i;
+for (i = 0; i < Object.keys(boxes).length; i++) {
+
+    newBox(boxes[Object.keys(boxes)[i]][0],boxes[Object.keys(boxes)[i]][1],Object.keys(boxes)[i]);
+}
+
 window.addEventListener("scroll", function (event) {
+    document.getElementById('ground').style.width = document.getElementById('bg').offsetWidth + "px";
     bob_walk();
     let scroll = this.scrollY;
-    document.getElementById('scrollScore').innerHTML = "score: " + Math.round(scroll/10) + "<br><button onclick='unmute'>Unmute!</button>";
+    document.getElementById('scrollScore').innerHTML = "score: " + Math.round(scroll/10);
     console.log(scroll);
     document.getElementById('ground').style.left = -scroll + "px";
+    document.getElementById('text').style.left = -scroll/2.2 + "px";
     bob.style.bottom = 135.5 + "px";
 
     //For loop for jumps
@@ -22,13 +41,16 @@ window.addEventListener("scroll", function (event) {
     for (i = 0; i < Object.keys(jumpies).length; i++) {
 
         //For each jump strength's position scroll
-        let ii = 0;
+        let ii;
         for (ii = 0; ii < jumpies[Object.keys(jumpies)[i]].length; ii++) {
             //jump function call for according variables
             jump(jumpies[Object.keys(jumpies)[i]][ii],Object.keys(jumpies)[i],scroll);
         }
 
     }
+
+
+
 
 
 
@@ -53,4 +75,16 @@ function jump(start,strength,scroll){
         bob.style.bottom = 135.5 + (-0.02 * Math.pow((scroll - scroll_start), 2) + strength * (scroll - scroll_start)) + "px";
         bob.style.backgroundImage = "url(./img/jump.png)";
     }
+}
+
+function newBox(left_,bottom_,key) {
+    let img = document.createElement('img');
+    img.src = "./img/box.png";
+    img.classList.add('box');
+    img.setAttribute("name",key);
+    img.style.left = left_ + "px";
+    img.style.bottom = bottom_ + 135.5 + "px";
+    document.getElementById('ground').appendChild(img);
+    // console.log('box made!');
+    // return "left:" + left + "bottom:" + bottom + ""
 }
