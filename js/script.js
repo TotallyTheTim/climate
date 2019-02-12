6bc90b4188a2360ef0d93870555cbd8dfa410a23
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2019-02-13 00:22:59 +0100

    update

--- js/script.js
+++ js/script.js
@@ -11,21 +11,22 @@ let bob = document.getElementById('bob');
 let bob_I = 0;
 let bob_left = bob.offsetLeft;
 let jumpies = {
+    3.1 : [180],
     5 : [3800,4980],
     6 : [980,1660],
     7 : [2150, 2910]
 };
 
 let boxes = {
-    box1 : [1085,203],
-    box2 : [5291,203]
+    box1 : [1085,203,"Did you know that Driving a car is not good for Climate change?"],
+    box2 : [5291,203,"Other Text here!"]
 };
 
 
 let i;
 for (i = 0; i < Object.keys(boxes).length; i++) {
 
-    newBox(boxes[Object.keys(boxes)[i]][0],boxes[Object.keys(boxes)[i]][1],Object.keys(boxes)[i]);
+    newBox(boxes[Object.keys(boxes)[i]][0],boxes[Object.keys(boxes)[i]][1],Object.keys(boxes)[i],boxes[Object.keys(boxes)[i]][2]);
 }
 
 window.addEventListener("scroll", function (event) {
@@ -44,6 +45,7 @@ window.addEventListener("scroll", function (event) {
     document.getElementById('text').style.left = -scroll/2.2 + "px";
     bob.style.bottom = 135.5 + "px";
 
+
     //For loop for jumps
     let i;
     //For each jump strength:
@@ -56,6 +58,16 @@ window.addEventListener("scroll", function (event) {
             jump(jumpies[Object.keys(jumpies)[i]][ii],Object.keys(jumpies)[i],scroll);
         }
 
+    }
+    
+    
+    
+    if (scroll > 654){
+        document.getElementById('box1').classList.add('boxUP');
+        document.getElementById('boxbox1').style.opacity = '1';
+    } else {
+        document.getElementById('box1').classList.remove('boxUP');
+        document.getElementById('boxbox1').style.opacity = '0';
     }
 
 
@@ -86,14 +98,24 @@ function jump(start,strength,scroll){
     }
 }
 
-function newBox(left_,bottom_,key) {
+function newBox(left_,bottom_,key,text) {
     let img = document.createElement('img');
+    let div = document.createElement('div');
+    div.innerText = text;
     img.src = "./img/box.png";
     img.classList.add('box');
-    img.setAttribute("name",key);
+    img.setAttribute("id",key);
+    div.setAttribute("id","box" + key);
     img.style.left = left_ + "px";
+    div.style.left = left_ + 100 + "px";
+    div.style.bottom = bottom_ + 235.5 + "px";
+    div.style.position = "absolute";
     img.style.bottom = bottom_ + 135.5 + "px";
+    div.style.background = "rgba(255,255,255,0.8)";
+    div.style.padding = "4px 8px";
+    div.style.opacity = "0";
     document.getElementById('ground').appendChild(img);
+    document.getElementById('ground').appendChild(div);
     // console.log('box made!');
     // return "left:" + left + "bottom:" + bottom + ""
 }
