2c042108520b7739dca6faec9ea5a549d841c8a6
HHK3 <joeltilon@live.nl>
2019-02-13 18:22:52 +0100

    Updated Files + New Pics

--- js/script.js
+++ js/script.js
@@ -10,16 +10,28 @@ window.scrollTo(0, 0);
 let bob = document.getElementById('bob');
 let bob_I = 0;
 let bob_left = bob.offsetLeft;
+let lastScrollPosition = 0;
 let jumpies = {
-    3.1 : [180],
-    5 : [3800,4980],
-    6 : [980,1660],
-    7 : [2150, 2910]
+    3.1 : [-316],
+    3.15 : [3890,5400],
+    5 : [3300,4490],
+    6 : [490,1150],
+    7 : [1685, 2420]
+    // 3.1 : [180],
+    // 3.15 : [4380,5900],
+    //
+    // 5 : [3800,4980],
+    //
+    // 6 : [980,1660],
+    //
+    // 7 : [2150, 2910]
+
 };
 
 let boxes = {
     box1 : [1085,203,"Did you know that Driving a car is not good for Climate change?"],
-    box2 : [5291,203,"Other Text here!"]
+    box2 : [5291,203,"Other Text here!"],
+    box3 : [6784,203,"Other Text LMAO!"]
 };
 
 
@@ -35,15 +47,28 @@ window.addEventListener("scroll", function (event) {
     document.getElementById('ground').style.width = document.getElementById('bg').offsetWidth + "px";
     document.getElementById('mountain').style.width = document.getElementById('bg').offsetWidth + "px";
     document.getElementById('ice').style.width = document.getElementById('bg').offsetWidth + "px";
-    bob_walk();
     let scroll = this.scrollY;
+    bob_walk(scroll);
     document.getElementById('scrollScore').innerHTML = "score: " + Math.round(scroll/10);
     console.log(scroll);
     document.getElementById('ground').style.left = -scroll + "px";
     document.getElementById('mountain').style.left = scroll/1.2 + "px";
+    document.getElementById('mountain2').style.left = scroll/1.2 + "px";
     document.getElementById('ice').style.left = scroll/1.3 + "px";
     document.getElementById('text').style.left = -scroll/2.2 + "px";
     bob.style.bottom = 135.5 + "px";
+
+    // var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    //
+    // scrollY <= this.lastScroll
+    //     ? console.log('up')
+    //     : console.log('down');
+    //
+    // this.lastScroll = scrollY ;
+    // var scrollX = window.pageXOffset || document.documentElement.scrollLeft;
+    // var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    //
+    // console.log('Scroll X:'+scrollX+' Scroll Y:'+scrollY);
 
 
     //For loop for jumps
@@ -62,10 +87,31 @@ window.addEventListener("scroll", function (event) {
     
     
     
-    if (scroll > 654){
+    if (scroll > 408){
         document.getElementById('box1').classList.add('boxUP');
         document.getElementById('boxbox1').style.opacity = '1';
-    } else {
+
+        if (scroll > 4614) {
+            document.getElementById('box2').classList.add('boxUP2');
+            document.getElementById('boxbox2').style.opacity = '1';
+
+            if (scroll > 6115) {
+                document.getElementById('box3').classList.add('boxUP');
+                document.getElementById('boxbox3').style.opacity = '1';
+            }
+
+            else {
+                document.getElementById('box3').classList.remove('boxUP');
+                document.getElementById('boxbox3').style.opacity = '0';
+            }
+        }
+
+        else {
+            document.getElementById('box2').classList.remove('boxUP2');
+            document.getElementById('boxbox2').style.opacity = '0';
+        }
+    }
+    else {
         document.getElementById('box1').classList.remove('boxUP');
         document.getElementById('boxbox1').style.opacity = '0';
     }
@@ -79,15 +125,34 @@ window.addEventListener("scroll", function (event) {
 
 });
 
-function bob_walk() {
-    if (bob_I < 10){
-        bob.style.backgroundImage = "url(./img/walk_1.png)";
-    } else if (bob_I > 19){
-        bob_I = 0;
-    } else {
-        bob.style.backgroundImage = "url(./img/walk_2.png)";
+function bob_walk(scroll) {
+
+    if (scroll < lastScrollPosition){
+        //upward - code here
+        if (bob_I < 10){
+            bob.style.backgroundImage = "url(./img/walk_1.png)";
+            bob.style.transform = "scaleX(-1)"
+        } else if (bob_I > 19){
+            bob_I = 0;
+        } else {
+            bob.style.backgroundImage = "url(./img/walk_2.png)";
+        }
+        bob_I++;
+    }else{
+        //downward - code here
+        if (bob_I < 10){
+            bob.style.backgroundImage = "url(./img/walk_1.png)";
+            bob.style.transform = "scaleX(1)"
+        } else if (bob_I > 19){
+            bob_I = 0;
+        } else {
+            bob.style.backgroundImage = "url(./img/walk_2.png)";
+        }
+        bob_I++;
     }
-    bob_I++;
+    lastScrollPosition = scroll;
+
+
 }
 
 function jump(start,strength,scroll){
@@ -113,9 +178,11 @@ function newBox(left_,bottom_,key,text) {
     img.style.bottom = bottom_ + 135.5 + "px";
     div.style.background = "rgba(255,255,255,0.8)";
     div.style.padding = "4px 8px";
+    div.style.fontSize = "25px";
     div.style.opacity = "0";
     document.getElementById('ground').appendChild(img);
     document.getElementById('ground').appendChild(div);
     // console.log('box made!');
     // return "left:" + left + "bottom:" + bottom + ""
 }
+
